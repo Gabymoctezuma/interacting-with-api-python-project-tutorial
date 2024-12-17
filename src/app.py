d6c0c5cae3b549fd2ea84416f606caf5f8acfa71
Gabymoctezuma <gabiiimp@hotmail.com>
2024-12-17 19:17:09 +0000

    analisis de datos spotify

--- src/app.py
+++ src/app.py
@@ -5,3 +5,12 @@ from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
+canciones=[]
+popularidad=[]
+duracion=[]
+
+for track in results['tracks'][:10]:
+    print('track    : ' + track['name'])
+    canciones.append(track['name'])
+    popularidad.append(track['popularity'])
+    duracion.append(track['duration_ms']/(1000*60)%60)
